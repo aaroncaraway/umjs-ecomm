da28689f56baefe4a383eaa03d3c16e630d1c5a7
aaroncaraway42 <aaroncaraway42@gmail.com>
2020-07-25 16:03:37 -0700

    First commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send(`<div>
+  <form method='POST'>
+    <input name="email" type="email" />
+    <input name="password" type="password" />
+    <input name="passwordConfirmation" type="password" />
+    <button>Submit</button>  
+  </form>
+  </div>`);
+});
+
+const bodyParser = (req, res, next) => {
+  if (req.method == "POST") {
+    req.on("data", (data) => {
+      const parsed = data.toString("utf8").split("&");
+      const formData = {};
+      for (let pair of parsed) {
+        const [key, value] = pair.split("=");
+        formData[key] = value;
+      }
+
+      req.body = formData;
+      next();
+    });
+  } else {
+    next();
+  }
+};
+
+app.post("/", bodyParser, (req, res) => {
+  console.log(req.body);
+  res.send("ACCOUNT CREATED!!");
+});
+
+app.listen(3000, () => {
+  console.log("listening!");
+});
