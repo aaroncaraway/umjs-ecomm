b81e0ebf27872fed7fd0204ae2a36019556d8822
aaroncaraway42 <aaroncaraway42@gmail.com>
2020-07-26 16:10:16 -0700

    S27 368 and 369 updating and deleting records, complete

--- repositories/users.js
+++ repositories/users.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const crypto = require("crypto");
 class UsersRepository {
   constructor(filename) {
     if (!filename) {
@@ -21,6 +22,7 @@ class UsersRepository {
   }
 
   async create(attrs) {
+    attrs.id = this.randomId();
     const records = await this.getAll();
     records.push(attrs);
     this.writeAll(records);
@@ -32,14 +34,36 @@ class UsersRepository {
       JSON.stringify(records, null, 2)
     );
   }
+
+  async getOne(id) {
+    const records = await this.getAll();
+    return records.find((id) => id === id);
+  }
+
+  async delete(id) {
+    const records = await this.getAll();
+    const updatedRecords = records.filter((record) => record.id !== id);
+    await this.writeAll(updatedRecords);
+  }
+
+  randomId() {
+    return crypto.randomBytes(4).toString("hex");
+  }
 }
-// const repo = new UsersRepository();
 
 const test = async () => {
   const repo = new UsersRepository("users.json");
-  await repo.create({ email: "catmom@gmail", password: "ilovecats" });
-  const users = await repo.getAll();
-  console.log(users);
+  //   TESTING CREATE + GET ALL
+  //   await repo.create({ email: "catmom@gmail", password: "ilovecats" });
+  //   const users = await repo.getAll();
+  //   console.log(users);
+
+  //  TESTING GETONE
+  //   const user = await repo.getOne("fbdeba1c");
+  //   console.log(user);
+
+  //  TESTING DELETE
+  await repo.delete("2675571a");
 };
 
 test();
