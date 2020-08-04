8e72f50358b18a0e079c510d7b529a0481b197b1
aaroncaraway42 <aaroncaraway42@gmail.com>
2020-08-04 14:41:25 -0700

    S29 403, completed

---
+++ repositories/repository.js
@@ -0,0 +1,84 @@
+const fs = require("fs");
+const crypto = require("crypto");
+const util = require("util");
+const scrypt = util.promisify(crypto.scrypt);
+
+module.exports = class Repository {
+  constructor(filename) {
+    if (!filename) {
+      throw new Error("Need filename!");
+    }
+
+    this.filename = filename;
+    try {
+      fs.accessSync(this.filename);
+    } catch (err) {
+      fs.writeFileSync(this.filename, "[]");
+    }
+  }
+  async getAll() {
+    return JSON.parse(
+      await fs.promises.readFile(this.filename, {
+        encoding: "utf8",
+      })
+    );
+  }
+
+  randomId() {
+    return crypto.randomBytes(4).toString("hex");
+  }
+
+  async create(attrs) {
+    attrs.id = this.randomId();
+    const records = await this.getAll();
+    records.push(attrs);
+
+    await this.writeAll(records);
+    return attrs;
+  }
+
+  async writeAll(records) {
+    await fs.promises.writeFile(
+      this.filename,
+      JSON.stringify(records, null, 2)
+    );
+  }
+
+  async getOne(id) {
+    const records = await this.getAll();
+    return records.find((id) => id === id);
+  }
+
+  async delete(id) {
+    const records = await this.getAll();
+    const updatedRecords = records.filter((record) => record.id !== id);
+    await this.writeAll(updatedRecords);
+  }
+
+  async update(id, attrs) {
+    const records = await this.getAll();
+    const record = records.find((record) => record.id == id);
+
+    if (!record) {
+      throw new Error(`Record with ${id} not found`);
+    }
+
+    Object.assign(record, attrs);
+    await this.writeAll(records);
+  }
+
+  async getOneBy(filters) {
+    const records = await this.getAll();
+    for (let record of records) {
+      let found = true;
+      for (let key in filters) {
+        if (record[key] !== filters[key]) {
+          found = false;
+        }
+      }
+      if (found) {
+        return record;
+      }
+    }
+  }
+};
--- repositories/users.js
+++ repositories/users.js
@@ -2,32 +2,9 @@ const fs = require("fs");
 const crypto = require("crypto");
 const util = require("util");
 const scrypt = util.promisify(crypto.scrypt);
+const Repository = require("./repository");
 
-class UsersRepository {
-  constructor(filename) {
-    if (!filename) {
-      throw new Error("Need filename!");
-    }
-
-    this.filename = filename;
-    try {
-      fs.accessSync(this.filename);
-    } catch (err) {
-      fs.writeFileSync(this.filename, "[]");
-    }
-  }
-  async getAll() {
-    return JSON.parse(
-      await fs.promises.readFile(this.filename, {
-        encoding: "utf8",
-      })
-    );
-  }
-
-  randomId() {
-    return crypto.randomBytes(4).toString("hex");
-  }
-
+class UsersRepository extends Repository {
   async create(attrs) {
     attrs.id = this.randomId();
     const records = await this.getAll();
@@ -51,51 +28,6 @@ class UsersRepository {
     console.log("HAAAY COLIN", suppliedHash_asBuf);
 
     return savedHash === suppliedHash_asBuf.toString("hex");
-  }
-
-  async writeAll(records) {
-    await fs.promises.writeFile(
-      this.filename,
-      JSON.stringify(records, null, 2)
-    );
-  }
-
-  async getOne(id) {
-    const records = await this.getAll();
-    return records.find((id) => id === id);
-  }
-
-  async delete(id) {
-    const records = await this.getAll();
-    const updatedRecords = records.filter((record) => record.id !== id);
-    await this.writeAll(updatedRecords);
-  }
-
-  async update(id, attrs) {
-    const records = await this.getAll();
-    const record = records.find((record) => record.id == id);
-
-    if (!record) {
-      throw new Error(`Record with ${id} not found`);
-    }
-
-    Object.assign(record, attrs);
-    await this.writeAll(records);
-  }
-
-  async getOneBy(filters) {
-    const records = await this.getAll();
-    for (let record of records) {
-      let found = true;
-      for (let key in filters) {
-        if (record[key] !== filters[key]) {
-          found = false;
-        }
-      }
-      if (found) {
-        return record;
-      }
-    }
   }
 }
 
