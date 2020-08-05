de46cf7403267455ceef240358c189d7a0c7f9c3
aaroncaraway42 <aaroncaraway42@gmail.com>
2020-08-04 17:03:45 -0700

    S30 413, complete

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const productsRepo = require("../../repositories/products");
+const multer = require("multer");
+
 const productsNewTemplate = require("../../views/admin/products/new");
 const { requireTitle, requirePrice } = require("../../routes/admin/validators");
 const { validationResult } = require("express-validator");
 
 const router = express.Router();
+const upload = multer({ storage: multer.memoryStorage() });
 
 router.get("/admin/products", (req, res) => {
   res.send("PRODUCT PAGE");
@@ -14,16 +17,23 @@ router.get("/admin/products/new", (req, res) => {
   res.send(productsNewTemplate({}));
 });
 
-router.post("/admin/products/new", [requireTitle, requirePrice], (req, res) => {
-  const errors = validationResult(req);
+router.post(
+  "/admin/products/new",
+  upload.single("image"),
+  [requireTitle, requirePrice],
+  async (req, res) => {
+    const errors = validationResult(req);
 
-  console.log(errors);
+    if (!errors.isEmpty()) {
+      return res.send(productsNewTemplate({ req, errors }));
+    }
+    const image = req.file.buffer.toString("base64");
+    const { title, price } = req.body;
 
-  //   if (!errors.isEmpty()) {
-  //     res.send(productsNewTemplate({ req, errors }));
-  //   }
+    await productsRepo.create({ title, price, image });
 
-  res.send("Product created successfully!");
-});
+    res.send("Product created successfully!");
+  }
+);
 
 module.exports = router;
--- views/admin/products/new.js
+++ views/admin/products/new.js
@@ -4,27 +4,30 @@ const { getError } = require("../../helpers");
 module.exports = ({ errors }) => {
   return layout({
     content: `
-      <div class="container">
-        <div class="columns is-centered">
-          <div class="column is-one-quarter">
-            <form method="POST">
-              <h1 class="title">Create Product</h1>
-              <div class="field">
-                <label class="label">Product Name</label>
-                <input required class="input" placeholder="Product Name" name="productName" />
-                <p class="help is-danger">${getError(errors, "productName")}</p>
-              </div>
-              <div class="field">
-                <label class="label">Price</label>
-                <input required class="input" placeholder="Price" name="price" />
-                <p class="help is-danger">${getError(errors, "price")}</p>
-              </div>
-              <div class="field">
-                <input type="file" name="image" />
-              </field>
-              <button class="button is-primary">Create Product</button>
-            </form>
-          </div>
+      <div class="columns is-centered">
+        <div class="column is-half">
+          <h1 class="subtitle">Create a Product</h1>
+
+          <form method="POST" enctype="multipart/form-data">
+            <div class="field">
+              <label class="label">Title</label>
+              <input class="input" placeholder="Title" name="title">
+              <p class="help is-danger">${getError(errors, "title")}</p>
+            </div>
+            
+            <div class="field">
+              <label class="label">Price</label>
+              <input class="input" placeholder="Price" name="price">
+              <p class="help is-danger">${getError(errors, "price")}</p>
+            </div>
+            
+            <div class="field">
+              <label class="label">Image</label>            
+              <input type="file" name="image" />
+            </div>
+            <br />
+            <button class="button is-primary">Create</button>
+          </form>
         </div>
       </div>
     `,
