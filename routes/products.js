4e5f85e694baaedd3fc3f8dcfed2dfc0b26aabd5
aaroncaraway42 <aaroncaraway42@gmail.com>
2020-08-06 17:20:09 -0700

    Listing products before video

--- routes/products.js
+++ routes/products.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const productsRepo = require("../repositories/products");
+const productsList = require("../views/products/index");
 
 const router = express.Router();
 
 router.get("/", async (req, res) => {
   const products = await productsRepo.getAll();
-  res.send(`<h1>${products[0].title}</h1>`);
+  res.send(productsList({ products }));
+  //   res.send(`<h1>${products[0].title}</h1>`);
 });
 
 module.exports = router;
---
+++ views/products/index.js
@@ -0,0 +1,6 @@
+module.exports = ({ products }) => {
+  const renderedProducts = products.map((product) => {
+    return `<li>${product.title} | ${product.price}</li>`;
+  });
+  return renderedProducts.join("");
+};
