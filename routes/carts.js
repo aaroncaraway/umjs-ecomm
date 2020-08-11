9f4f6388a095a3602d29bdc7ca13af238e1b55dd
aaroncaraway42 <aaroncaraway42@gmail.com>
2020-08-10 18:09:38 -0700

    S31, 436 working

--- routes/carts.js
+++ routes/carts.js
@@ -1,8 +1,37 @@
 const express = require("express");
+const cartsRepo = require("../repositories/carts");
 
 const router = express.Router();
+
+router.post("/cart/products", async (req, res) => {
+  console.log(req.body.productId);
+
+  let cart;
+  if (!req.session.cartId) {
+    cart = await cartsRepo.create({ items: [] });
+    req.session.cartId = cart.id;
+  } else {
+    cart = await cartsRepo.getOne(req.session.cartId);
+  }
+
+  const existingItem = cart.items.find(
+    (item) => item.id === req.body.productId
+  );
+
+  if (existingItem) {
+    existingItem.quantity++;
+  } else {
+    cart.items.push({ id: req.body.productId, quantity: 1 });
+  }
+
+  await cartsRepo.update(cart.id, { items: cart.items });
+  console.log(cart);
+
+  res.send("Product added to cart!");
+});
 
 router.get("/mycart", (req, res) => {
   res.send("MY CART!!");
 });
+
 module.exports = router;
